c95c59b8d58c362854399f43b03326773900fef1
Kusuma <ktejaswini403@gmail.com>
2024-04-17 21:41:51 +0530

    Solved Progran 5 in java assignment 1

--- src/main/java/JavaAssigment1/Program5_NumbersSquare.java
+++ src/main/java/JavaAssigment1/Program5_NumbersSquare.java
@@ -3,6 +3,22 @@ package JavaAssigment1;
 public class Program5_NumbersSquare {
 
     public void numberSquare() {
-
+        for (int i = 0; i < 7; i++) //rows
+        {
+            for (int j = 0; j < 7; j++) //numbers
+            {
+                if (i == 0 || i == 6 || j == 0 || j == 6) {
+                    System.out.print("4");
+                } else if (i == 1 || i == 5 || j == 1 || j == 5)
+                    System.out.print("3");
+                else if (i == 2 || i == 4 || j == 2 || j == 4)
+                    System.out.print("2");
+                else
+                    System.out.print("1");
+            }
+            System.out.println();
+        }
     }
 }
+
+
